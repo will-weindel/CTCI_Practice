fd34440a0c904e13bb193cb0e337fcc3104f42ed
Will Weindel <will.weindel@gmail.com>
2021-07-27 20:03:50 -0700

    adds IOCE to URLify

--- Ch 1 - Arrays and Strings/3-URLify/URLify.js
+++ Ch 1 - Arrays and Strings/3-URLify/URLify.js
@@ -1,5 +1,14 @@
 //Write a method to replace all spaces in a string with '%20'.
 
+//I - string
+//O - modified string with %20
+//C - do we need to check for data types? non URLs?
+//E - none
+
+//D - string
+//A/P - for loop / loop through string, create new string
+//T - N/A
+
 const URLify = (str) => {
   //You write code here
 };
