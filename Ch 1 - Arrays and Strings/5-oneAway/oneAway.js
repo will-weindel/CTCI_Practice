4ad5fc2e9bd4ece608aad9dd5a5c8c2e28d6c0c0
Will Weindel <will.weindel@gmail.com>
2021-07-30 17:09:01 -0700

    add IOCE to oneAway

--- Ch 1 - Arrays and Strings/5-oneAway/oneAway.js
+++ Ch 1 - Arrays and Strings/5-oneAway/oneAway.js
@@ -1,5 +1,15 @@
 //There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
 
+//I - two strings
+//O - bool
+//C - single words, spaces (trimming), check for data types
+//E - none
+
+//Ds - strings greater than 1 diff in length
+//Da - n/a
+//Al - recursion
+//T - n/a
+
 function oneAway(str1, str2) {
   //take your code, and put it here
 }
