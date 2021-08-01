2d6f83944cad7930ea9df8e743c055c33d3e1092
Will Weindel <will.weindel@gmail.com>
2021-07-31 17:27:37 -0700

    add IOCE to stringComp

--- Ch 1 - Arrays and Strings/6-stringCompression/stringCompression.js
+++ Ch 1 - Arrays and Strings/6-stringCompression/stringCompression.js
@@ -1,5 +1,15 @@
 //Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return the original string. You can assume the string has only uppercase and lowercase letters (a - z).
 
+//I - string
+//O - sring (compressed or not)
+//C - Upper and lower
+//E - empty
+
+//D - no letters, one letter
+//D - n/a
+//A - for loop
+//T - n/a
+
 function stringCompression(str) {
   //I'd put the code here
 }
