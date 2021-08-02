1d225211cc41443a38f0f29d77ddfeb4e50868c8
Will Weindel <will.weindel@gmail.com>
2021-08-02 16:53:10 -0700

    add solution to ZeroMatrix

--- Ch 1 - Arrays and Strings/8-zeroMatrix/zeroMatrix.js
+++ Ch 1 - Arrays and Strings/8-zeroMatrix/zeroMatrix.js
@@ -15,8 +15,34 @@
 //loop through each element in array
   //if 0, helper function will 0-out col and row
 
-function zeroMatrix(matrix) {
-  //The void goes here
-}
+  function zeroMatrix(matrix) {
+    let numberOfRows = matrix.length - 1;
+    let numberOfCols = matrix[0].length - 1;
+
+    for (var i = 0; i <= numberOfRows; i++) {
+      for (var j = 0; j <= numberOfCols; j++) {
+        if (matrix[i][j] === 0) {
+          zeroOutRowsAndCols(i, j, numberOfRows, numberOfCols, matrix);
+        }
+      }
+    }
+
+    for (var i = 0; i <= numberOfRows; i++) {
+      for (var j = 0; j <= numberOfCols; j++) {
+        if (matrix[i][j] === '0') matrix[i][j] = 0;
+      }
+    }
+    return matrix;
+  }
+
+  function zeroOutRowsAndCols(row, col, numberOfRows, numberOfCols, matrix) {
+    for (var i = 0; i <= numberOfCols; i++) {
+      matrix[row][i] = '0';
+    }
+    for (var j = 0; j <= numberOfRows; j++) {
+      matrix[j][col] = '0';
+    }
+    return;
+  }
 
 module.exports = zeroMatrix;
