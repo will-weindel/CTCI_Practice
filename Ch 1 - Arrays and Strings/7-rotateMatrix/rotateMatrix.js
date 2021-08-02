2cf14e6d10d9468ff641b319e6722c4591253c27
Will Weindel <will.weindel@gmail.com>
2021-08-02 16:19:53 -0700

    considers IOCE for rotateMatrix

--- Ch 1 - Arrays and Strings/7-rotateMatrix/rotateMatrix.js
+++ Ch 1 - Arrays and Strings/7-rotateMatrix/rotateMatrix.js
@@ -1,5 +1,15 @@
 //Given an image represented by an N x N matrix, where each pixel in the image is represented by an integer, write a method to rotate the image by 90 degrees. Can you do this in place?
 
+//I - N X N matrix
+//O - rotatedMatrix (90)
+//C - Disquals? Not NxN? Unique numbers
+//E - no input, wrong input etc.
+
+//Dis - N/A
+//Da - N/A
+//A-P - double for-loop
+//T - n/a
+
 function rotateMatrix(matrix) {
   //EVERYTHING MUST ROTATE!
 }
