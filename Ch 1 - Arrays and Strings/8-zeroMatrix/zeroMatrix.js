56a1e6aab3b78ee9b9b45b874e6294421cafd2af
Will Weindel <will.weindel@gmail.com>
2021-08-02 16:51:36 -0700

    add IOCE to zeroMatrix

--- Ch 1 - Arrays and Strings/8-zeroMatrix/zeroMatrix.js
+++ Ch 1 - Arrays and Strings/8-zeroMatrix/zeroMatrix.js
@@ -1,5 +1,15 @@
 //Write an algorithm such that if an element in an M x N matrix is 0, its entire row and column are set to 0.
 
+//I - MxN matrix (possible 0's);
+//O - modified Matrix
+//C - filtering? matrix irregularies
+//E - all 0's, empty?
+
+//DQ's -
+//DS's - n/a - we will work in place
+//AP's - for-loop w/ helper function
+//TR's - n/a
+
 function zeroMatrix(matrix) {
   //The void goes here
 }
