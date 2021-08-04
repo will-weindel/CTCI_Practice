fc01a9428e64dc80a047e2820eebaf6493af3dd9
Will Weindel <will.weindel@gmail.com>
2021-08-04 16:58:34 -0700

    add IOCE to deleteMiddle.js

--- Ch 2 - Linked Lists/3-deleteMiddle/deleteMiddle.js
+++ Ch 2 - Linked Lists/3-deleteMiddle/deleteMiddle.js
@@ -15,9 +15,18 @@ function Node(val) {
 }
 */
 
+//I - LL
+//O - n/a (just remove the node)
+//C - time or space?  methods? how are we limited?
+//E - are we always given the node?
+
+//DQ - n/a
+//DS - n/a
+//AP - n/a
+//TR - n/a
 
 const deleteMiddle = (node) => {
-  node.data = node.next.data;
+  node.val = node.next.val;
   node.next = node.next.next;
 };
 
