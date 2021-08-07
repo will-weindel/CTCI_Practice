d303e0e0ce9915eb5be4570eb3ba051e3b13d6c2
Will Weindel <will.weindel@gmail.com>
2021-08-07 16:36:39 -0700

    add IOCE to intersection.js

--- Ch 2 - Linked Lists/7-intersection/intersection.js
+++ Ch 2 - Linked Lists/7-intersection/intersection.js
@@ -17,13 +17,35 @@ function Node(val) {
   this.next = null;
 }
 */
+
+// I - Two pointers to LL heads
+// O - If true, same node | else false
+// C - Time O(n) | Space O(n) | Only nodes?
+// E - null heads,
+
+
 function Node(val) {
   this.val = val;
   this.next = null;
 }
 
 const intersection = (head1, head2) => {
-  //Please code here
+  let currentNodeListOne = head1;
+  let currentNodeListTwo = head2;
+  let listOneNodeMap = new Map();
+
+  while (currentNodeListOne) {
+    listOneNodeMap.set(currentNodeListOne, true);
+    currentNodeListOne = currentNodeListOne.next;
+  }
+
+  while (currentNodeListTwo) {
+    if (listOneNodeMap.has(currentNodeListTwo)) {
+      return currentNodeListTwo;
+    }
+    currentNodeListTwo = currentNodeListTwo.next;
+  }
+  return false;
 };
 
 
