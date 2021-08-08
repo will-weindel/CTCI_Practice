714233c626b1e41bbc0b47b277da184ab9ec4bfe
Will Weindel <will.weindel@gmail.com>
2021-08-07 18:55:02 -0700

    add IOCE to loopDetection.js

--- Ch 2 - Linked Lists/8-loopDetection/loopDetection.js
+++ Ch 2 - Linked Lists/8-loopDetection/loopDetection.js
@@ -14,13 +14,41 @@ function Node(val) {
   this.next = null;
 }
 */
+
+// I - ref to head of LL
+// O - ref to loop node in LL | or false
+// C - Time O(N) | Space O(1) | upper limit | lower limit
+// E/DQ's - null head, only one node, can node link to itself?
+
+
 function Node(val) {
   this.val = val;
   this.next = null;
 }
 
 const loopDetection = (head) => {
-  //Please code here
+  let slowRunner = head;
+  let fastRunner = head;
+
+  while (fastRunner) {
+    if (fastRunner.next === null || fastRunner.next.next === null) {
+      return false;
+    }
+    fastRunner = fastRunner.next.next;
+    slowRunner = slowRunner.next;
+    if (fastRunner === slowRunner) {
+      slowRunner = head;
+      break;
+    }
+  }
+
+  while (true) {
+    if (slowRunner === fastRunner) {
+      return slowRunner;
+    }
+    slowRunner = slowRunner.next;
+    fastRunner = fastRunner.next;
+  }
 };
 
 
