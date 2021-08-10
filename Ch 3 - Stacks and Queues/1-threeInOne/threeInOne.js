24d83ea84de24597c6a0d36c7430a697cc0e3668
Will Weindel <will.weindel@gmail.com>
2021-08-09 18:38:42 -0700

    add initial solution to threeInOne.js

--- Ch 3 - Stacks and Queues/1-threeInOne/threeInOne.js
+++ Ch 3 - Stacks and Queues/1-threeInOne/threeInOne.js
@@ -18,51 +18,99 @@ var ThreeInOne = function () {
 };
 
 ThreeInOne.prototype.push1 = function (value) {
-  //
+  this.container.push('1#' + String(value));
 };
 
 ThreeInOne.prototype.push2 = function (value) {
-  //
+  this.container.push('2#' + String(value));
 };
 
 ThreeInOne.prototype.push3 = function (value) {
-  //
+  this.container.push('3#' + String(value));
 };
 
 ThreeInOne.prototype.pop1 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '1' && this.container[i].charAt(1) === '#') {
+      let stackVal = Number(this.container[i].substring(2));
+      this.container.splice(i, 1);
+      return stackVal;
+    }
+  }
 };
 
 ThreeInOne.prototype.pop2 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '2' && this.container[i].charAt(1) === '#') {
+      let stackVal = Number(this.container[i].substring(2));
+      this.container.splice(i, 1);
+      return stackVal;
+    }
+  }
 };
 
-ThreeInOne.prototype.pop3 = function (value) {
-  //
+ThreeInOne.prototype.pop3 = function () {
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '3' && this.container[i].charAt(1) === '#') {
+      let stackVal = Number(this.container[i].substring(2));
+      this.container.splice(i, 1);
+      return stackVal;
+    }
+  }
 };
 
 ThreeInOne.prototype.peek1 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '1' && this.container[i].charAt(1) === '#') {
+      let stackVal = Number(this.container[i].substring(2));
+      return stackVal;
+    }
+  }
 };
 
 ThreeInOne.prototype.peek2 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '2' && this.container[i].charAt(1) === '#') {
+      let stackVal = Number(this.container[i].substring(2));
+      return stackVal;
+    }
+  }
 };
 
 ThreeInOne.prototype.peek3 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '3' && this.container[i].charAt(1) === '#') {
+      let stackVal = Number(this.container[i].substring(2));
+      return stackVal;
+    }
+  }
 };
 
 ThreeInOne.prototype.isEmpty1 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '1' && this.container[i].charAt(1) === '#') {
+      return false;
+    }
+  }
+  return true;
 };
 
 ThreeInOne.prototype.isEmpty2 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '2' && this.container[i].charAt(1) === '#') {
+      return false;
+    }
+  }
+  return true;
 };
 
 ThreeInOne.prototype.isEmpty3 = function () {
-  //
+  for (var i = this.container.length - 1; i >= 0; i--) {
+    if (this.container[i].charAt(0) === '3' && this.container[i].charAt(1) === '#') {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = ThreeInOne;
