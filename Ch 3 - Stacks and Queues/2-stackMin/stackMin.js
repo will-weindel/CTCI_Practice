698b1e3474625c976bfdad886607be710a2a33aa
Will Weindel <will.weindel@gmail.com>
2021-08-10 19:15:53 -0700

    add IOCE to stackMin.js

--- Ch 3 - Stacks and Queues/2-stackMin/stackMin.js
+++ Ch 3 - Stacks and Queues/2-stackMin/stackMin.js
@@ -1,5 +1,10 @@
 //How would you design a stack which, in addition to push and pop, has a function min which returns the minimum element? push, pop and min should all operate in 0(1) time.
 
+// I - Build a stack class which can track the minimum value
+// O - standard pop and min output
+// C - Time O(1) for all methods | Space O(n) | min and max value
+// E/DQs - non numeric
+
 var Stack = require('./../util/Stack');
 
 class StackMin extends Stack {
