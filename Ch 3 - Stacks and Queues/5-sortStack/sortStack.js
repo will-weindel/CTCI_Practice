03455a2b63b8b002e71a44afd8a129642b6c4a99
Will Weindel <will.weindel@gmail.com>
2021-08-13 15:29:38 -0700

    add IOCE to sortStack.js

--- Ch 3 - Stacks and Queues/5-sortStack/sortStack.js
+++ Ch 3 - Stacks and Queues/5-sortStack/sortStack.js
@@ -1,9 +1,50 @@
 //Write a program to sort a stack such that the smallest items are on the top. You can use an additional temporary stack, but you may not copy the elements into any other data structure (such as an array).
 
+// I - a stack
+// O - a sorted stack
+// C - Time complexity? Space Complexity? range of numbers
+// E-DQs - stack with one value, stack with no value, not a stack
+
 var Stack = require('./../util/Stack');
 
 var sortStack = function (stack) {
-  //code!
+  let numberOfIterations = stack.size() - 1;
+  let tempStack = new Stack();
+
+  for (var i = 0; i <= numberOfIterations; i += 2) {
+    let smallestValue = transitionStack(stack, tempStack, numberOfIterations - i);
+    let secondSmallestValue = transitionStack(tempStack, stack, numberOfIterations - i - 1);
+    tempStack.push(smallestValue);
+    tempStack.push(secondSmallestValue);
+  }
+  sortToMainStack(tempStack, stack, tempStack.size() - 1);
+  return stack;
 };
+
+var transitionStack = (fromStack, toStack, iterations) => {
+  let value;
+  for (var i = 0; i <= iterations; i++) {
+    let temp = fromStack.pop();
+    if (!value) {
+      value = temp;
+    } else if (temp < value) {
+      toStack.push(value);
+      value = temp;
+    } else {
+      toStack.push(temp);
+    }
+  }
+  return value;
+}
+
+var sortToMainStack = (fromStack, toStack, iterations) => {
+  if (!fromStack.peek()) {
+    fromStack.pop();
+    iterations--;
+  }
+  for (var i = 0; i <= iterations; i++) {
+    toStack.push(fromStack.pop());
+  }
+}
 
 module.exports = sortStack;
