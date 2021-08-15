00270d5a3df3cde3f0d024308869dd57b3f91b97
Will Weindel <will.weindel@gmail.com>
2021-08-14 18:38:18 -0700

    add IOCE to routeBetween.js

--- Ch 4 - Trees and Graphs/1-routeBetweenNodes/routeBetweenNodes.js
+++ Ch 4 - Trees and Graphs/1-routeBetweenNodes/routeBetweenNodes.js
@@ -1,10 +1,48 @@
 //Given a directed graph, design an algorithm to find out whether there is a route between two nodes.
 
+// I - two node references, one graph reference
+// O - boolean (based on route)
+// C - Time Comp | Space Comp | size of graph, type of graph, graph values (odd nodes)
+// E - node does not exist, non node value
+
 var Graph = require('./../util/Graph');
 var Queue = require('./../util/Queue');
 
 function routeBetweenNodes(value1, value2, graph) {
-  //magic computer instructions go here
+  if (!graph.hasNode(value1)) return false;
+  if (!graph.hasNode(value2)) return false;
+
+  let firstNode = value1;
+  let secondNode = value2;
+
+  if (searchForTargetNode(firstNode, secondNode, new Queue(), {firstNode}, graph)) return true;
+  if (searchForTargetNode(secondNode, firstNode, new Queue(), {secondNode}, graph)) return true;
+
+  return false;
+}
+
+function addNewNodesToQueue(rootNode, queue, cache, graph) {
+  for (let node in graph.findEdges(rootNode)) {
+    if (!cache[node]) {
+      queue.enqueue(node);
+      cache[node] = true;
+    }
+  }
+  return;
+}
+
+function searchForTargetNode(rootNode, target, queue, cache, graph) {
+  addNewNodesToQueue(rootNode, queue, cache, graph);
+  while (true) {
+    let node = queue.dequeue();
+    if (node === target) {
+      return true;
+    } else {
+      addNewNodesToQueue(node, queue, cache, graph)
+    }
+    if (queue.isEmpty()) break;
+  }
+  return false;
 }
 
 module.exports = routeBetweenNodes;
