5a4ed77b6f26810c7276625a10ad78a6b8357e14
Will Weindel <will.weindel@gmail.com>
2021-08-14 19:01:40 -0700

    updates IOCE and DS&A for isUnique.js

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -1,12 +1,21 @@
 //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
-//I - a string
-//O - boolean
-//C - greater than 26 letters? (only if single word)
-//E - spaces? empty strings?
-//D - HashTable or object with key
-//A/P - double for loop
-//T - sort and check neighbor?
+// I - string (non sorted, does not exist in external data structure)
+// O - boolean
+// C - Time O(nLogn) | Space O(1) | str. length
+// E - char case, unique values, spaces, non char values
+
+// DS - we could use hash, also possible to solve w/o
+// AP - double for loop | single for loop + hashmap | sort first, then for loop
+// TR - sort the str
+
+// sort the str
+// loop through each value in str (if neighbor is same value, return false)
+// return true
+
+// create hashmap
+// loop through each value in str
+  // add each value to hashmap (if value already exists, return false)
 
 function isUnique(str) {
   let count = 1;
