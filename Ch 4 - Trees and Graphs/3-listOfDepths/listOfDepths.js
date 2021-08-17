57e2f7802c5ecee0efe6e3302865e165c6df37cc
Will Weindel <will.weindel@gmail.com>
2021-08-16 20:02:15 -0700

    add IOCE to listOfDepths.js

--- Ch 4 - Trees and Graphs/3-listOfDepths/listOfDepths.js
+++ Ch 4 - Trees and Graphs/3-listOfDepths/listOfDepths.js
@@ -1,5 +1,10 @@
 //Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
 
+// I - bst at root
+// O - parent LL (each node's value prop contains pointer to sub-LL)
+// C - Time O(n) | Space O(n) | min/max nodes in bst
+// E/DQs - handle null values? odd node values (like LL)
+
 var BST = require('./../util/BST');
 var LinkedList = require('./../util/LinkedList');
 var Queue = require('./../util/Queue');
