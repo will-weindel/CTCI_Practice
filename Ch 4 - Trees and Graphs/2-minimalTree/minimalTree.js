8b6687e5d072591a5a87a26d7bc269e10a8d47a3
Will Weindel <will.weindel@gmail.com>
2021-08-16 19:24:23 -0700

    add IOCE to minimalTree.js

--- Ch 4 - Trees and Graphs/2-minimalTree/minimalTree.js
+++ Ch 4 - Trees and Graphs/2-minimalTree/minimalTree.js
@@ -1,10 +1,37 @@
 //Given a sorted (increasing order) array with unique integer elements, write an algorithm to create a binary search tree with minimal height.
 
+// I - sorted array
+// O - reference to BST root
+// C - Time O(n) | O(n) | min/max array length
+// E/DQs - n/a
+
 var Queue = require('./../util/Queue');
 var BST = require('./../util/BST');
 
 var insertBalanced = function (array) {
-  //little tree goes here
+  let middle = Math.ceil(array.length / 2) - 1;
+  let binarySearchTree = new BST(array[middle]);
+
+  treeify(array.slice(0, middle), binarySearchTree);
+  treeify(array.slice(middle + 1), binarySearchTree);
+
+  return binarySearchTree;
+
 };
+
+function treeify(array, tree) {
+  if (!array.length) return null;
+  if (array.length === 1) return tree.insert(array[0]);
+
+  let middle = Math.ceil(array.length / 2) - 1;
+  let leftArray = array.slice(0, middle);
+  let rightArray = array.slice(middle + 1);
+
+  tree.insert(array[middle]);
+  treeify(leftArray, tree);
+  treeify(rightArray, tree);
+
+  return tree;
+}
 
 module.exports = insertBalanced;
