70719a9d70aeadce0fb5a76d1a48430b27100b4f
Will Weindel <will.weindel@gmail.com>
2021-08-17 19:23:18 -0700

    add IOCE to checkBalanced.js

--- Ch 4 - Trees and Graphs/4-checkBalanced/checkBalanced.js
+++ Ch 4 - Trees and Graphs/4-checkBalanced/checkBalanced.js
@@ -1,5 +1,10 @@
 //Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
 
+// I - bst root
+// O - boolean
+// C - Time | Space | min height, max height
+// E - n/a
+
 var BST = require('./../util/BST');
 
 var checkBalanced = function (bst) {
