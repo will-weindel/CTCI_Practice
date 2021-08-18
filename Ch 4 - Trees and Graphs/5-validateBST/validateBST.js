5ae30aedaa647c9bf6989881a7c21df2b66806ce
Will Weindel <will.weindel@gmail.com>
2021-08-17 20:52:26 -0700

    add IOCE to validateBSt.js

--- Ch 4 - Trees and Graphs/5-validateBST/validateBST.js
+++ Ch 4 - Trees and Graphs/5-validateBST/validateBST.js
@@ -6,8 +6,28 @@ var BinaryTree = function (value) {
   this.right = null;
 };
 
+// I - Binary Tree
+// O - boolean based on bstness
+// C - Time O(n) | Space O(1) | min max tree size, min max vals, repeats?
+// E - single bt, non bt
+
 var validateBST = function (bt) {
-  //got code?
+  let leftWithinRange = valueWithinRange(bt.left, -Infinity, bt.value);
+  let rightWithinRange = valueWithinRange(bt.right, bt.value, Infinity);
+  if (!leftWithinRange || !rightWithinRange) return false;
+  return true;
 };
+
+var valueWithinRange = function (bt, lowerLimit, upperLimit) {
+  if (!bt) return true;
+  if (bt.value > upperLimit) return false;
+  if (bt.value < lowerLimit) return false;
+
+  let leftWithinRange = valueWithinRange(bt.left, lowerLimit, bt.value);
+  let rightWithinRange = valueWithinRange(bt.right, bt.value, upperLimit);
+
+  if (!leftWithinRange || !rightWithinRange) return false;
+  return true;
+}
 
 module.exports = validateBST;
