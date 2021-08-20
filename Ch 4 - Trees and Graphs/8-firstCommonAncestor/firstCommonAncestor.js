21dc84cd9d9b9a1beeb6287bd21978fed161ab70
Will Weindel <will.weindel@gmail.com>
2021-08-19 19:05:02 -0700

    add IOCE to firstCommonAncestor.js

--- Ch 4 - Trees and Graphs/8-firstCommonAncestor/firstCommonAncestor.js
+++ Ch 4 - Trees and Graphs/8-firstCommonAncestor/firstCommonAncestor.js
@@ -15,8 +15,45 @@ BinaryTree.prototype.isAncestor = function (node2) {
   //you can write this one too
 };
 
+// I - two nodes
+// O - a node (possibly null)
+// C - Time O(n) | Space O(1)
+// E - single node (same node), nodes from different trees?
+
 var firstCommonAncestor = function (node1, node2) {
-  //you know what to do
+  const nodeOnePathLength = getPathLength(node1);
+  const nodeTwoPathLength = getPathLength(node2);
+  const difference = Math.abs(nodeOnePathLength - nodeTwoPathLength);
+  let nodeWithGreaterPath = nodeOnePathLength >= nodeTwoPathLength ? node1 : node2;
+  let nodeWithSmallerPath = nodeOnePathLength >= nodeTwoPathLength ? node2 : node1;
+
+  nodeWithGreaterPath = traverseNodePath(nodeWithGreaterPath, difference);
+
+  while (nodeWithSmallerPath) {
+    if (nodeWithSmallerPath === nodeWithGreaterPath) {
+      return nodeWithSmallerPath.value;
+    }
+    nodeWithSmallerPath = nodeWithSmallerPath.parent;
+    nodeWithGreaterPath = nodeWithGreaterPath.parent;
+  }
+  return null;
 };
+
+const getPathLength = function(node) {
+  let count = 0;
+  let parentNode = node.parent;
+  while (parentNode) {
+    count++;
+    parentNode = parentNode.parent;
+  }
+  return count;
+}
+
+const traverseNodePath = function(node, length) {
+  for (let i = 1; i <= length; i++) {
+    node = node.parent;
+  }
+  return node;
+}
 
 module.exports = firstCommonAncestor;
