0d2e697e45af51a183743de0fecbdff95d4253e2
Will Weindel <will.weindel@gmail.com>
2021-08-20 12:08:14 -0700

    add IOCE to checkSubTree.js

--- Ch 4 - Trees and Graphs/10-checkSubtree/checkSubtree.js
+++ Ch 4 - Trees and Graphs/10-checkSubtree/checkSubtree.js
@@ -12,12 +12,45 @@ var Tree = function (value) {
   this.right = null;
 };
 
+// I - two tree nodes
+// O - bool
+// C - Time O(n) | Space O(n)
+// E - n/a
+
 var isSame = function (tree1, tree2) {
-  //helper function
+  let treeOneStartingNode = recurseTreeOne(tree1, tree2);
+  if (treeOneStartingNode) {
+    return recurseBothTrees(treeOneStartingNode, tree2);
+  } else {
+    return false;
+  }
 };
 
-Tree.prototype.isSubtree = function (tree2) {
-  //main method
-};
+const recurseTreeOne = function(node1, node2) {
+  if (!node1) return;
+  if (node1.value === node2.value) return node1;
+
+  let leftNode = recurseTreeOne(node1.left, node2);
+  let rightNode = recurseTreeOne(node1.right, node2);
+
+  if (leftNode) return leftNode;
+  if (rightNode) return rightNode;
+  return null;
+}
+
+const recurseBothTrees = function(node1, node2) {
+  if (!node1 && !node2) return true;
+  if (!node1 || !node2) return false;
+  if (node1.value !== node2.value) return false;
+
+  let leftNodes = recurseBothTrees(node1.left, node2.left);
+  let rightNodes = recurseBothTrees(node1.right, node2.right);
+
+  if (!leftNodes) return false;
+  if (!rightNodes) return false;
+
+  return true;
+}
+
 
 module.exports = Tree;
