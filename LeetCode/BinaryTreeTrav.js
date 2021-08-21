99ab70e802a84c5ee54bd4fed55d2f8051401849
Will Weindel <will.weindel@gmail.com>
2021-08-21 12:34:00 -0700

    add iterative approach to inorder traversal

--- LeetCode/BinaryTreeTrav.js
+++ LeetCode/BinaryTreeTrav.js
@@ -30,3 +30,30 @@ var traverseTree = function(node, array) {
   traverseTree(node.right, array);
   return;
 };
+
+//Below is an iterative approach to in-order traversal (normally done with recursion)
+//It uses a stack (which mimics the recursive stack)
+// ** Mis: I tend to avoid setting a variable to null (i.e. setting node = node.left when resulting in null)
+  // you do not need to avoid this all the time (your base case or recursive case can handle it)
+
+var inorderTraversal = function(root) {
+  if (!root) return [];
+  if (!root.left && !root.right) return [root.val];
+
+  let traversalStack = [];
+  let inorderValues = [];
+  let currentNode = root;
+
+  while (currentNode || traversalStack.length) {
+    if (currentNode) {
+      traversalStack.push(currentNode);
+      currentNode = currentNode.left;
+    } else {
+        currentNode = traversalStack.pop();
+        inorderValues.push(currentNode.val);
+        currentNode = currentNode.right;
+    }
+  }
+
+  return inorderValues;
+}
