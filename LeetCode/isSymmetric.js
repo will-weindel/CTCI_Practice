92901cd285a0af3649659a61a1de8b27fae5c485
Will Weindel <will.weindel@gmail.com>
2021-08-21 14:20:59 -0700

    adds additional recursive helper

--- LeetCode/isSymmetric.js
+++ LeetCode/isSymmetric.js
@@ -1,3 +1,8 @@
+// Below is a recursive approach to finding if a binary tree is symmetric
+// it checks the leftNode's left against the rightNode's right and vice versa
+// ** Mis - not adding multiple ||'s to an if statement
+// ** Mis - used wrong property and also '==';
+
 var isSymmetric = function(root) {
   if (!root) return false;
   return checkNodesAreSymmetric(root.left, root.right);
@@ -13,3 +18,14 @@ const checkNodesAreSymmetric = function(leftNode, rightNode) {
   if (!outerCheck || !innerCheck) return false;
   return true;
 }
+
+//the below CheckNodes func also works for recursion
+
+// const checkNodesAreSymmetric = function(leftNode, rightNode) {
+//   if (!leftNode && !rightNode) return true;
+//   if (!leftNode || !rightNode || leftNode.val !== rightNode.val) return false;
+//   return checkNodesAreSymmetric(leftNode.left, rightNode.right) &&
+//          checkNodesAreSymmetric(leftNode.right, rightNode.left);
+// }
+
+// notice the return statement uses a && to pass bool info to previous call
