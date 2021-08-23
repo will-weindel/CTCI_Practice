568123bd229821b429cbc7b03f859da9ada7bcc8
Will Weindel <will.weindel@gmail.com>
2021-08-23 16:16:27 -0700

    add comments to stairs.js

--- Ch 8 - Dynamic Programmin/stairs.js
+++ Ch 8 - Dynamic Programmin/stairs.js
@@ -9,3 +9,23 @@
 // DS - hashmap (obj) as cache
 // AP - top down recursion
 // TR - n/a
+
+// this approach caches an initial value (it could cache more)
+// the return statement uses a check to avoid null/NaN error
+
+const walkingUpStairs = function(n) {
+  const stairPaths = {
+    0: 1
+  };
+
+  const stepDown = function(n) {
+    if (!stairPaths[n - 1]) {
+      stairPaths[n - 1] = stepDown(n - 1);
+    };
+    return (stairPaths[n - 1] ? stairPaths[n - 1] : 0) +
+           (stairPaths[n - 2] ? stairPaths[n - 2] : 0) +
+           (stairPaths[n - 3] ? stairPaths[n - 3] : 0);
+  }
+
+  return stepDown(n);
+}
