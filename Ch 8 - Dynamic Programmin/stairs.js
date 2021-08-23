2a5cad5805a14fd58e15166cb188cadd08df1bbe
Will Weindel <will.weindel@gmail.com>
2021-08-23 16:14:11 -0700

    add DS&A to stairs.js

--- Ch 8 - Dynamic Programmin/stairs.js
+++ Ch 8 - Dynamic Programmin/stairs.js
@@ -2,3 +2,10 @@
 // O - n representing the number of paths to top
 // C - Time O(n) using memo | Time O(2^n) w/o memo | Space O(C + Hs)
 // E - no broken stairs, at least one stair
+
+// use memoize strategy | cache path values
+// recurse top-down
+
+// DS - hashmap (obj) as cache
+// AP - top down recursion
+// TR - n/a
