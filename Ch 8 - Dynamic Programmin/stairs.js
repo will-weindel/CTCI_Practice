c7e115b34f407c2f3efc9e3fe16408c1cd703ebb
Will Weindel <will.weindel@gmail.com>
2021-08-23 16:12:23 -0700

    add IOCE to stairs.js

---
+++ Ch 8 - Dynamic Programmin/stairs.js
@@ -0,0 +1,4 @@
+// I - n representing a number of stairs
+// O - n representing the number of paths to top
+// C - Time O(n) using memo | Time O(2^n) w/o memo | Space O(C + Hs)
+// E - no broken stairs, at least one stair
