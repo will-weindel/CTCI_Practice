f3e3cec9490767749e70b702ddac70a7d7f0ca26
Will Weindel <will.weindel@gmail.com>
2021-08-24 16:08:51 -0700

    add pseudocode and DS&A to magic.js

--- Ch 8 - Dynamic Programmin/3-magic/magic.js
+++ Ch 8 - Dynamic Programmin/3-magic/magic.js
@@ -2,3 +2,14 @@
 // O - index or null
 // C - Time O(LogN) | Space O(LogN)
 // E - Array w/o unique values
+
+// create index cache
+// begin in the middle of the array
+ // if index value is less than element value, look in first half
+ // else look in second half
+ // continue on a LogN pattern until found, or index repeated in cache (meaning no magic index)
+
+ // DS - hashtable (cache)
+ // AP - iterative - binary search (log search)
+
+ // comp - cache | index (P) | halfLenght
