2a086f03856936e32d1d2e82fbfbb15d717386bc
Will Weindel <will.weindel@gmail.com>
2021-08-24 13:05:57 -0700

    add pseudocode and DS&A's

--- Ch 8 - Dynamic Programmin/2-robot/robot.js
+++ Ch 8 - Dynamic Programmin/2-robot/robot.js
@@ -2,3 +2,11 @@
 // O - new matrix
 // C - Time O(n) | Space O(2LengthDiag);
 // E - can not reach
+
+// create a copy of the input
+// create a queue (for each new space)
+// loop through queue (until empty)
+  // for each position 1) add new positions to queue, mark space on copy of input
+
+// DS - queue, matrix
+// AP - iteration (while), memoizing with matrix
