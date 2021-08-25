19d1db8ed165381607087fa5655b4cf5a818751e
Will Weindel <will.weindel@gmail.com>
2021-08-25 14:21:20 -0700

    add pseudocode to multiply.js

--- Ch 8 - Dynamic Programmin/5-multiply/multiply.js
+++ Ch 8 - Dynamic Programmin/5-multiply/multiply.js
@@ -2,3 +2,9 @@
 // O - product of both integers
 // C - can not use *, Time O(2LogN) | Space O(LogN)
 // E - num is 0, num is 1, num is neg, num is not num
+
+// double a base-2 value until value is greater than first input
+  // while doubling, cache base-2 value and doubled value of second input (after first use)
+// once base-2 value is greater than first input, loop backwards through cache,
+  //add cache vals to accum., subtract base vals from original num
+// return accum.
