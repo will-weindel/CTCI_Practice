90ccbd25d75bfbe7bc95a946fc043c942178de7b
Will Weindel <will.weindel@gmail.com>
2021-08-24 19:50:46 -0700

    add initial solution to subset.js

--- Ch 8 - Dynamic Programmin/4-Subsets/subsets.js
+++ Ch 8 - Dynamic Programmin/4-Subsets/subsets.js
@@ -8,3 +8,18 @@
 // i.e. [] -> [], [1], -> [], [1], [2], [1, 2]
 // basically you are taking what is already there, add a new value to all items, then
   // adding both groups back together
+
+  const getSubsets = function(set) {
+    let currentSubsetCache = [[], [set[0]]];
+
+    for (let i = 1; i < set.length; i++) {
+      let nextSubsetCache = [];
+      for (let subset of currentSubsetCache) {
+        let newSubset = subset.slice(0);
+        newSubset.push(set[i]);
+        nextSubsetCache.push(subset, newSubset);
+      }
+      currentSubsetCache = nextSubsetCache;
+    }
+    return currentSubsetCache;
+  }
