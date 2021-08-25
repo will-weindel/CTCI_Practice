7c9037f8637a23ecee8f772a123676520ecc0430
Will Weindel <will.weindel@gmail.com>
2021-08-24 19:49:27 -0700

    add psuedocode to subset.js

--- Ch 8 - Dynamic Programmin/4-Subsets/subsets.js
+++ Ch 8 - Dynamic Programmin/4-Subsets/subsets.js
@@ -2,3 +2,9 @@
 // O - a list of all subsets
 // C - Time O(2^n) | Space O(2^n)
 // E - n/a
+
+// general idea is to use memoizing.
+// memoizing works by taking the values of the smaller subset and adding one new value to them
+// i.e. [] -> [], [1], -> [], [1], [2], [1, 2]
+// basically you are taking what is already there, add a new value to all items, then
+  // adding both groups back together
