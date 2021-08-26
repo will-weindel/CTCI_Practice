8eade841299d145a0ff72ad225a7350765b3f607
Will Weindel <will.weindel@gmail.com>
2021-08-26 11:02:48 -0700

    adds pseudocode to hanoi.js

--- Ch 8 - Dynamic Programmin/6-hanoi.js/hanoi.js
+++ Ch 8 - Dynamic Programmin/6-hanoi.js/hanoi.js
@@ -3,3 +3,9 @@
 // C - Time (w/o memo) is O(2^n)
 // E - 0 towers
 
+// create a Towers Of Hanoi class (factory function) with 3 stacks
+// create a move method
+// create an initialize method
+// move method will have a recurse function which takes a val, source tower, buffer tower and desintation tower.
+// begin at base case and work backwards
+
