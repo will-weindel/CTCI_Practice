37988022d1d3dd8384256a4336fb4ad25dc112d1
Will Weindel <will.weindel@gmail.com>
2021-08-27 13:04:31 -0700

    add IOCE to fillColor.js

---
+++ Ch 8 - Dynamic Programmin/10-fillColor/fillColor.js
@@ -0,0 +1,4 @@
+// I - matrix, click location, new color
+// O - return the original matrix (update in place)
+// C - Time O(n) | Space O(1)
+// E - clicked on value already is color | no matrix | matrix size 1
