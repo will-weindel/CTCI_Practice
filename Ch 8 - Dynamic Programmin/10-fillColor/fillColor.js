5b6d344bff2c0c3e51c3ce40f73aa4efe2e9a760
Will Weindel <will.weindel@gmail.com>
2021-08-27 13:06:13 -0700

    add pseudocode to fillColor.js

--- Ch 8 - Dynamic Programmin/10-fillColor/fillColor.js
+++ Ch 8 - Dynamic Programmin/10-fillColor/fillColor.js
@@ -2,3 +2,7 @@
 // O - return the original matrix (update in place)
 // C - Time O(n) | Space O(1)
 // E - clicked on value already is color | no matrix | matrix size 1
+
+// use BFS to 1) change colors of nearest relevant neighbors,
+// use queue to update all colors in a given area
+
