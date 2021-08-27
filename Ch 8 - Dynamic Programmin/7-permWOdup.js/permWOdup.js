c0ce74965c89f729de14bb67a6c302b2ded96ae0
Will Weindel <will.weindel@gmail.com>
2021-08-26 17:37:53 -0700

    add initial solution to permWOdup.js

--- Ch 8 - Dynamic Programmin/7-permWOdup.js/permWOdup.js
+++ Ch 8 - Dynamic Programmin/7-permWOdup.js/permWOdup.js
@@ -7,3 +7,31 @@
 // the new letter should be added before and after each letter of the previous perms
 // be sure to add new perms to the end of the cache (saving the previous cache)
 
+const permutationsOfAString = function(string) {
+  if (!string.length) return [''];
+  if (string.length === 1) return ['', string];
+
+  let substringCache = [''];
+
+  for (let i = 0; i < string.length; i++) {
+    addLetterToPermutations(string[i], substringCache)
+  }
+
+  return substringCache;
+}
+
+const addLetterToPermutations = function(letter, substringCache) {
+  let cacheLength = substringCache.length;
+
+  for (let i = 0; i < cacheLength; i++) {
+    for (let j = 0; j <= substringCache[i].length; j++) {
+      substringCache.push(substringCache[i].substring(0, j) + letter + substringCache[i].substring(j))
+    }
+  }
+}
+
+// TEST
+
+console.log(permutationsOfAString('abc'));
+console.log(permutationsOfAString('abcd'));
+console.log(permutationsOfAString('abcde'));
