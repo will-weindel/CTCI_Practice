87b6d7c108e75213ad79255bf509370d7642330f
Will Weindel <will.weindel@gmail.com>
2021-08-26 17:36:38 -0700

    add pseudocode to permWOdup.js

--- Ch 8 - Dynamic Programmin/7-permWOdup.js/permWOdup.js
+++ Ch 8 - Dynamic Programmin/7-permWOdup.js/permWOdup.js
@@ -2,3 +2,8 @@
 // O - all permutations
 // C - Time (extreme, worse than factorial) | Space (worse than factorial)
 // E - n/a
+
+// loop through previous set of permutations and add a new letter to each perm
+// the new letter should be added before and after each letter of the previous perms
+// be sure to add new perms to the end of the cache (saving the previous cache)
+
