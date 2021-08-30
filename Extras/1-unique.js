f2f376370b01a52ec668314cd401038d9515a29c
Will Weindel <will.weindel@gmail.com>
2021-08-30 11:34:01 -0700

    add additional 1-isUnique

--- Extras/1-unique.js
+++ Extras/1-unique.js
@@ -7,3 +7,12 @@ const isUnique = function(string) {
   }
   return true;
 }
+
+const isUnique2 = function(string) {
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j < string.length; j++) {
+      if (string[i] === string[j]) return false;
+    }
+  }
+  return true;
+}
