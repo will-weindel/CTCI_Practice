f251969245432f0618633402fcce2bc60fbdf41c
Will Weindel <will.weindel@gmail.com>
2021-08-31 14:27:36 -0700

    add sumList

--- Extras/2-all.js
+++ Extras/2-all.js
@@ -81,7 +81,7 @@ const partitionLinkedList = function(value, list) {
   if (!list.head) return null;
   if (!list.head.next) return list;
 
-let previousNode = list.head;
+  let previousNode = list.head;
   let currentNode = list.head.next;
 
   while (currentNode) {
@@ -99,4 +99,36 @@ let previousNode = list.head;
   }
 
   return list;
+}
+
+const sumLists = function(list1, list2) {
+  if (!list1.head) return list2;
+  if (!list2.head) return list1;
+
+  let currentNodeList1 = list1.head;
+  let currentNodeList2 = list2.head;
+  let carryover = 0;
+  let summedList = new LinkedList();
+
+  while (currentNodeList1 || currentNodeList2 || carryover) {
+    let sum = 0;
+    sum += currentNodeList1 ? currentNodeList1.value : 0;
+    sum += currentNodeList2 ? currentNodeList2.value : 0;
+    sum += carryover ? carryover : 0;
+
+    if (carryover) {
+      carryover = 0;
+    }
+
+    if (sum >= 10) {
+      sum -= 10;
+      carryover = 1;
+    }
+
+    summedList.addToTail(new Node(sum));
+    currentNodeList1 = currentNodeList1 ? currentNodeList1.next : null;
+    currentNodeList2 = currentNodeList2 ? currentNodeList2.next : null;
+  }
+
+  return summedList;
 }
