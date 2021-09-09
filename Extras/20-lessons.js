22ccf4efa16e667d0d5996c9ca43e971f98ab84c
Will Weindel <will.weindel@gmail.com>
2021-09-09 12:51:35 -0700

    add additional lesson comment

--- Extras/20-lessons.js
+++ Extras/20-lessons.js
@@ -1,5 +1,7 @@
 // I need to learn bit manipulation and bit vectors
 // Overall I think things went well. I am able to solve most of these problems in 30 mins or less.
 // The struggle is in problems that a so large it is hard to even think through a solution.
-// I think reviewing solution on LeetCode is a good idea (the upvoted solutions)
-// Many leetcode solutions are very good, short and appear optimal.
+// I think reviewing solutions on LeetCode is a good idea (the upvoted solutions)
+// Many (top rated) leetcode solutions are very good, short and appear optimal.
+// I have a plan for the next few weeks - mainly math, and the next few relavent chapters of CTCI
+// I'll need to think of a way to continue to add commits.
