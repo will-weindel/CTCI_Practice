69d73c4b7a19702fbe599f94696ae2b2103ea9dd
Will Weindel <will.weindel@gmail.com>
2021-09-09 12:49:05 -0700

    add additional lesson comment

--- Extras/20-lessons.js
+++ Extras/20-lessons.js
@@ -1,1 +1,3 @@
 // I need to learn bit manipulation and bit vectors
+// Overall I think things went well. I am able to solve most of these problems in 30 mins or less.
+// The struggle is in problems that a so large it is hard to even think through a solution.
