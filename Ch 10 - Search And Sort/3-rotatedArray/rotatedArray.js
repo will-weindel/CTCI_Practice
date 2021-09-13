085ea1a91233088c7362e14a8aaf2259e303f48c
Will Weindel <will.weindel@gmail.com>
2021-09-13 14:06:39 -0700

    add initial Solution to rotatedArray.js

--- Ch 10 - Search And Sort/3-rotatedArray/rotatedArray.js
+++ Ch 10 - Search And Sort/3-rotatedArray/rotatedArray.js
@@ -6,3 +6,33 @@
 // create three pointers (p) => low, mid, high
 // check which half of array contains number
 // update (p)'s to new low, mid, high index
+
+const findIndexInRotatedArray = function(array, value) {
+  let low = 0;
+  let high = array.length - 1;
+  let mid = Math.floor((high + low) / 2);
+
+  while (low <= high) {
+    if (array[mid] === value) return mid;
+
+    if (array[mid] < array[high]) {
+      if (value > array[mid] && value <= array[high]) {
+        low = mid + 1;
+        mid = Math.floor((high + low) / 2);
+      } else {
+        high = mid - 1;
+        mid = Math.floor((high + low) / 2);
+      }
+    } else {
+      if (value < array[mid] && value >= array[low]) {
+        high = mid - 1;
+        mid = Math.floor((high + low) / 2);
+      } else {
+        low = mid + 1;
+        mid = Math.floor((high + low) / 2);
+      }
+    }
+  }
+
+  return 'No index found.';
+}
