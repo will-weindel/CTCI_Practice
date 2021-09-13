1f8ea372cb99d8c613fae8106bc3b89147fdb53c
Will Weindel <will.weindel@gmail.com>
2021-09-13 13:18:52 -0700

    add initialSolution to groupAnagram.js

--- Ch 10 - Search And Sort/2-GroupAnagrams/groupAnagram.js
+++ Ch 10 - Search And Sort/2-GroupAnagrams/groupAnagram.js
@@ -6,3 +6,26 @@
 // loop through each item of input, add sorted version to cache
 // if any sorted item exists in cache -> add item index to key/value array
 // loop through cache -> build new array from indexs
+
+const groupAllAnagrams = function(arr) {
+  let anagramCache = {};
+  let groupedAnagrams = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let sortedString = arr[i].split('').sort().join('');
+    if (anagramCache[sortedString]) {
+      anagramCache[sortedString].push(i);
+    } else {
+      anagramCache[sortedString] = [];
+      anagramCache[sortedString].push(i);
+    }
+  }
+
+  for (let anagram in anagramCache) {
+    for (let index of anagramCache[anagram]) {
+      groupedAnagrams.push(arr[index]);
+    }
+  }
+
+  return groupedAnagrams;
+}
