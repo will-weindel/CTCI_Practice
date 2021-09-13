d7cf18722a4144f3bc12f4a488a398cf33aea546
Will Weindel <will.weindel@gmail.com>
2021-09-13 12:44:42 -0700

    add pseudocode to sortMerge.js

--- Ch 10 - Search And Sort/sortMerge.js
+++ Ch 10 - Search And Sort/sortMerge.js
@@ -2,3 +2,7 @@
 // O - one merged/sorted array
 // C - are all values unique? all ints? return new array?
 // E - diff data types
+
+// two pointer -> one at beginning of each array
+// compare (p) vals, push smaller val into new array
+// update pointers
