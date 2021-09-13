0ec0d1093cd816dd1a5256f0564323fd5dfc699e
Will Weindel <will.weindel@gmail.com>
2021-09-13 13:18:25 -0700

    add pseudocode to groupAnagram.js

--- Ch 10 - Search And Sort/2-GroupAnagrams/groupAnagram.js
+++ Ch 10 - Search And Sort/2-GroupAnagrams/groupAnagram.js
@@ -2,3 +2,7 @@
 // O - new array of anagrams
 // C - all strings? Time? Space?
 // E - Empty array
+
+// loop through each item of input, add sorted version to cache
+// if any sorted item exists in cache -> add item index to key/value array
+// loop through cache -> build new array from indexs
