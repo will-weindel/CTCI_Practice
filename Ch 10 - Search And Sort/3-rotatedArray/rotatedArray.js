ae1ff7e10c6382c3b33e6e64148f907df97bd8d1
Will Weindel <will.weindel@gmail.com>
2021-09-13 14:06:00 -0700

    add psuedocode to rotatedArray.js

--- Ch 10 - Search And Sort/3-rotatedArray/rotatedArray.js
+++ Ch 10 - Search And Sort/3-rotatedArray/rotatedArray.js
@@ -2,3 +2,7 @@
 // O - index of value
 // C - Time Space Vals?
 // E - empty, does not exist?
+
+// create three pointers (p) => low, mid, high
+// check which half of array contains number
+// update (p)'s to new low, mid, high index
