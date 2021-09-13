e05ab37b4330aee5bd457e53226ecd70ed936fcb
Will Weindel <will.weindel@gmail.com>
2021-09-13 12:45:11 -0700

    add initial solution to sortMerge.js

--- Ch 10 - Search And Sort/sortMerge.js
+++ Ch 10 - Search And Sort/sortMerge.js
@@ -6,3 +6,32 @@
 // two pointer -> one at beginning of each array
 // compare (p) vals, push smaller val into new array
 // update pointers
+
+const mergeTwoArrays = function(arr1, arr2) {
+  if (!arr1.length) return arr2;
+  if (!arr2.length) return arr1;
+
+  let arr1Index = 0;
+  let arr2Index = 0;
+  const mergedArray = [];
+
+  while (arr1[arr1Index] || arr2[arr2Index]) {
+    if (!arr1[arr1Index]) {
+      mergedArray.push(arr2[arr2Index]);
+      arr2Index++;
+    }
+    else if (!arr2[arr2Index]) {
+      mergedArray.push(arr1[arr1Index]);
+      arr1Index++;
+    }
+    else if (arr1[arr1Index] < arr2[arr2Index]) {
+      mergedArray.push(arr1[arr1Index]);
+      arr1Index++;
+    } else {
+      mergedArray.push(arr2[arr2Index]);
+      arr2Index++;
+    }
+  }
+
+  return mergedArray;
+}
