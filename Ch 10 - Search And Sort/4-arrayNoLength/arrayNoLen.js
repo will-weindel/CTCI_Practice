5402ba375b25232d4caf1f7910f5c398abba3b44
Will Weindel <will.weindel@gmail.com>
2021-09-14 12:25:32 -0700

    add pseudo and walkthrough to arrayNoLen.js

--- Ch 10 - Search And Sort/4-arrayNoLength/arrayNoLen.js
+++ Ch 10 - Search And Sort/4-arrayNoLength/arrayNoLen.js
@@ -2,3 +2,12 @@
 //O - index of value
 //C - Time O(logN) Space O(1);
 //E - empty list
+
+//* 1. handle edge cases / sanity checks
+//* 2. find length of array by checking index returns against multiples of 2
+//* 3. use binary search (low, mid, high) once length is found
+
+// [1, 2, 3, 4, 5, 6, 7] -1, -1,
+// low = 5
+// high = 8
+// mid = 6
