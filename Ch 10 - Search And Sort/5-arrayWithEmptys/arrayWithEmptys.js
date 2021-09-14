eafe8a9ec85560f79600cd4d286509fd6f9b95fa
Will Weindel <will.weindel@gmail.com>
2021-09-14 14:26:17 -0700

    add binary search example to  arrayWithEmpties.js

--- Ch 10 - Search And Sort/5-arrayWithEmptys/arrayWithEmptys.js
+++ Ch 10 - Search And Sort/5-arrayWithEmptys/arrayWithEmptys.js
@@ -2,4 +2,35 @@
 // However, worst case scenary is O(n). It could be argued that for sake of clarity
 // and code maintainance, a simple linear search would suffice.
 // Additionally, a recurse BS will use extra space (in stack).
-// An iterative approach may be better
+// An iterative approach may be
+
+// the below code implements a recurse BS, (does not solve the problem).
+
+const findStringIndexInSparseArray = function(array, num) {
+
+  let low = 0;
+  let high = array.length - 1;
+  let mid = Math.floor((high + low) / 2);
+
+  const recurseFSIISA = function(low, mid, high) {
+    if (low > high) return null;
+    if (array[mid] === num) return mid;
+
+    if (num < array[mid]) {
+      high = mid - 1;
+      mid = Math.floor((high + low) / 2);
+      let leftCheck = recurseFSIISA(low, mid, high);
+      if (leftCheck !== null) return leftCheck;
+    }
+    else {
+      low = mid + 1;
+      mid = Math.floor((high + low) / 2);
+      let rightCheck = recurseFSIISA(low, mid, high);
+      if (rightCheck !== null) return rightCheck;
+    }
+
+    return null;
+  }
+
+  return recurseFSIISA(low, mid, high);
+}
