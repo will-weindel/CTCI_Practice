91dd448109b8add1deb1727e2dc67218c9bedd14
Will Weindel <will.weindel@gmail.com>
2021-09-14 12:32:02 -0700

    add solution to arrayNoLen.js

--- Ch 10 - Search And Sort/4-arrayNoLength/arrayNoLen.js
+++ Ch 10 - Search And Sort/4-arrayNoLength/arrayNoLen.js
@@ -11,3 +11,35 @@
 // low = 5
 // high = 8
 // mid = 6
+
+const findIndexInModifiedArray = function(array, value) {
+  if (array[0] === undefined) return null;
+
+  let index = 1;
+
+  while (array[index]) {
+    index *= 2;
+  }
+
+  let low = 0;
+  let high = index;
+  let mid = Math.floor((high + low) / 2);
+
+  while (low <= high && array[low] !== undefined) {
+    if (array[mid] === value) return mid;
+    else if (!array[mid]) {
+      high = mid;
+      mid = Math.floor((high + low) / 2);
+    }
+    else if (value > array[mid]) {
+      low = mid + 1;
+      mid = Math.floor((high + low) / 2);
+    }
+    else {
+      high = mid - 1;
+      mid = Math.floor((high + low) / 2);
+    }
+  }
+
+  return 'No index found.';
+}
