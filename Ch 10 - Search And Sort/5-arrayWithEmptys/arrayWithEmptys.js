2662e6adeb5b29f6500e472fbb0afd8799ac93c0
Will Weindel <will.weindel@gmail.com>
2021-09-14 14:25:25 -0700

    add description to arrayWithEmpties.js

---
+++ Ch 10 - Search And Sort/5-arrayWithEmptys/arrayWithEmptys.js
@@ -0,0 +1,5 @@
+// The general idea is to use a modified Binary Search to look through the array
+// However, worst case scenary is O(n). It could be argued that for sake of clarity
+// and code maintainance, a simple linear search would suffice.
+// Additionally, a recurse BS will use extra space (in stack).
+// An iterative approach may be better
