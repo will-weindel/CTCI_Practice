e68d0750f0dbfa7786d3d405f2cf09d68ba92caf
Will Weindel <will.weindel@gmail.com>
2021-09-15 14:49:12 -0700

    add problem setup to bigFile.js

--- Ch 10 - Search And Sort/6-bigFile/bigFile.js
+++ Ch 10 - Search And Sort/6-bigFile/bigFile.js
@@ -7,3 +7,16 @@
 // merge sort the information in memory, and write back to file (replace)
 // take small amounts of each sorted chunk, and merge sort into a new file
 // (basically your're taking chunks of chunks and merge sorting, once a sub-chunk is empty, get a new one)
+
+let largeFile = [];
+let externalFile = [];
+let sortedFile = [];
+let numberCache = {};
+
+while (largeFile.length < 500) {
+  let randomNum = Math.ceil(Math.random() * 500);
+  if (!numberCache[randomNum]) {
+    largeFile.push(randomNum);
+    numberCache[randomNum] = true;
+  }
+}
