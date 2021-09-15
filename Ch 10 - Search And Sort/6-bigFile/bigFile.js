57f69df9de9bda6ed80fa552727ef20785328e3f
Will Weindel <will.weindel@gmail.com>
2021-09-15 14:48:33 -0700

    add pseudocode to bigFile.js

--- Ch 10 - Search And Sort/6-bigFile/bigFile.js
+++ Ch 10 - Search And Sort/6-bigFile/bigFile.js
@@ -2,3 +2,8 @@
 // O - the large file sorted
 // C - total memory of comp
 // E - n/a
+
+// read chunks of the file into memory (maybe 1 GB at a time)
+// merge sort the information in memory, and write back to file (replace)
+// take small amounts of each sorted chunk, and merge sort into a new file
+// (basically your're taking chunks of chunks and merge sorting, once a sub-chunk is empty, get a new one)
