318f204b508fac644efd0fffaf49705e197f9a2d
Will Weindel <will.weindel@gmail.com>
2021-09-15 18:25:50 -0700

    add AddNode methods to AVL

--- Extras/avlTree.js
+++ Extras/avlTree.js
@@ -8,3 +8,65 @@ class AVLNode {
     this.rightCount = 0;
   }
 }
+
+class AVLTree {
+  constructor() {
+    this.rootNode = null;
+  }
+
+  addNode(value) {
+    let node = new AVLNode(value);
+
+    if (!this.rootNode) {
+      this.rootNode = node;
+      return null;
+    }
+
+    let currentNode = this.rootNode;
+
+    while (currentNode) {
+      if (value < currentNode) {
+        currentNode.leftCount++;
+        if (!currentNode.left) {
+          currentNode.left = node;
+          node.parent = currentNode;
+          break;
+        } else {
+          currentNode = currentNode.left;
+        }
+      } else {
+        currentNode.rightCount++;
+        if (!currentNode.right) {
+          currentNode.right = node;
+          node.parent = currentNode;
+          break;
+        } else {
+          currentNode = currentNode.right;
+        }
+      }
+    }
+
+    let unbalancedNode = this.checkBalance(currentNode);
+
+    if (unbalancedNode) {
+      let rotationMethod = this.determineRotationMethod(unbalancedNode, currentNode);
+
+      switch (rotationMethod) {
+        case 1:
+          this.rotateLL(unbalancedNode);
+          break;
+        case 2:
+          this.rotateLR(unbalancedNode);
+          break;
+        case 3:
+          this.rotateRR(unbalancedNode);
+          break;
+        case 4:
+          this.rotateRL(unbalancedNode);
+          break;
+        default: break;
+      }
+    }
+  }
+
+}
