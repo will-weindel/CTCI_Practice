38a3edd8b8b97dfcb33c43e3e9a71cd35c1dd978
Will Weindel <will.weindel@gmail.com>
2021-09-16 13:45:16 -0700

    add IOCE to rankStream

---
+++ Ch 10 - Search And Sort/10-rankStream/rankStream.js
@@ -0,0 +1,5 @@
+// I - a stream of numbers, and a look up value
+// O - a rank (sum of all numbers less than or equal to - but not including self - given lookup)
+// C - Time O Log N | Space Linear
+// E - N/A
+
