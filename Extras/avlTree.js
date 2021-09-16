903db94272e381139aa2a8bcf6b8db3197d2b55c
Will Weindel <will.weindel@gmail.com>
2021-09-15 18:24:41 -0700

    add avlTree Node

---
+++ Extras/avlTree.js
@@ -0,0 +1,10 @@
+class AVLNode {
+  constructor(value){
+    this.value = value;
+    this.parent = null;
+    this.left = null;
+    this.leftCount = 0;
+    this.right = null;
+    this.rightCount = 0;
+  }
+}
