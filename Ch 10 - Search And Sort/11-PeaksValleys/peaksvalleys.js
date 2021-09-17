5c201ca76bc779f89335605da532da3cfc3e5ec9
Will Weindel <will.weindel@gmail.com>
2021-09-17 13:31:07 -0700

    add pseudo to peakValley.js

--- Ch 10 - Search And Sort/11-PeaksValleys/peaksvalleys.js
+++ Ch 10 - Search And Sort/11-PeaksValleys/peaksvalleys.js
@@ -2,3 +2,7 @@
 // O - peak valley array (done in place)
 // C - Time O(n) | Space (O(1))
 // E - array less that length 2
+
+// 1. start at beginning of array
+// 2. check neighbor values for greater than or less than (depends on vector)
+// 3. swap values if they do not meet criteria
