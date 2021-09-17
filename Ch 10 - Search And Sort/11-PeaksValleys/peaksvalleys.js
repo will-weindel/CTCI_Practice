1f61084e543b1c185126c54a67c12fa2594fb2fa
Will Weindel <will.weindel@gmail.com>
2021-09-17 13:31:51 -0700

    add walkthrough to peakValley.js

--- Ch 10 - Search And Sort/11-PeaksValleys/peaksvalleys.js
+++ Ch 10 - Search And Sort/11-PeaksValleys/peaksvalleys.js
@@ -6,3 +6,9 @@
 // 1. start at beginning of array
 // 2. check neighbor values for greater than or less than (depends on vector)
 // 3. swap values if they do not meet criteria
+
+// input = [5, 3, 1, 2, 3] -> [5, 1, 3, 2, 3]
+// transformVector = t
+// i = 4
+// temp =
+
