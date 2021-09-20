12935c2f75fb0ba38a81a97210cb62b137508f04
Will Weindel <will.weindel@gmail.com>
2021-09-20 14:59:10 -0700

    add first solution to bitFind.js

--- LeetCode/bitFind.js
+++ LeetCode/bitFind.js
@@ -10,3 +10,16 @@
 // there are two ways we can approach a solution (one violates a contraint on space)
 // we can loop through the array, add each item to memory, and then if item exists, remove from mem
 // we can also use ^= to XOR a number to some accumulator, and the net result will be the only remaining num
+
+const findUniqueValueExtraMem = function(array) {
+  const valueMap = new Map();
+
+  for (let num of array) {
+    if (valueMap.has(num)) valueMap.delete(num);
+    else valueMap.set(num, 0);
+  }
+
+  for (let [key, value] of valueMap) {
+   return key;
+  }
+}
