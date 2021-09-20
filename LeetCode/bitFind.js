4f9e34196f5b658372a3fa4c03389ee538e641c6
Will Weindel <will.weindel@gmail.com>
2021-09-20 14:56:26 -0700

    add IOCE to bitFind.js

--- LeetCode/bitFind.js
+++ LeetCode/bitFind.js
@@ -1,3 +1,10 @@
 //Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
 //You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+// I - an array with a unique value (all other vals have pairs)
+// O - the unique number
+// C - Time O(n) | Space O(1) | number range | length range | type range
+// E - none number types
+
+
