efdcf8eecc9a7f8280ebaad214f550152d00390c
Will Weindel <will.weindel@gmail.com>
2021-09-20 17:54:20 -0700

    add pseudo solution to numberOfBinOnes.js

--- LeetCode/numberOfBinOnes.js
+++ LeetCode/numberOfBinOnes.js
@@ -5,3 +5,6 @@
 // O - the number of 1's in the nums binary value
 // C - Time O(n) | Space O(1)
 // E - not a number
+
+// if you '&' the number with one, it will return 1 if there is a 1 in the 2^0 (1's) place
+// use conditional to find 1 in 1's place, then >>= 1 to right shift the number by 1 bit, repeat
