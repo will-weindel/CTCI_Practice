3b8cf21c25e2f6805884aed43483bb3cb15d47f2
Will Weindel <will.weindel@gmail.com>
2021-09-21 14:26:57 -0700

    add IOCE to insertion.js

--- Ch 5 - Bit Manipulation/1-Insertion/insertion.js
+++ Ch 5 - Bit Manipulation/1-Insertion/insertion.js
@@ -8,6 +8,14 @@ EXAMPLE
 Input: N 10000000000, M = 10011, i 2, j 6
 Output: N = 10001001100
 */
+
+// I - two numbers (non-binary), i & j for range values
+// O - a number that has the smaller number add to it in range
+// C - Time | Space
+// E - can't fit in range?
+
+
+
 const insertion = (N, M, i, j) => {
   //insert bits here
 };
