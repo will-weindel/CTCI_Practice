baf7ba9e97d310faa41b2e043a711d858648206f
Will Weindel <will.weindel@gmail.com>
2021-09-20 17:52:48 -0700

    add description and IOCE to numberOfBinOnes.js

---
+++ LeetCode/numberOfBinOnes.js
@@ -0,0 +1,7 @@
+// Write a function that takes an unsigned integer and returns the number of '1' bits
+// it has (also known as the Hamming weight).
+
+// I - a number
+// O - the number of 1's in the nums binary value
+// C - Time O(n) | Space O(1)
+// E - not a number
