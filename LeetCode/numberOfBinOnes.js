c54db7320b3fc4255a95da91c5cb29161a52fc28
Will Weindel <will.weindel@gmail.com>
2021-09-20 17:54:50 -0700

    add initial solution to numberOfBinOnes.js

--- LeetCode/numberOfBinOnes.js
+++ LeetCode/numberOfBinOnes.js
@@ -8,3 +8,15 @@
 
 // if you '&' the number with one, it will return 1 if there is a 1 in the 2^0 (1's) place
 // use conditional to find 1 in 1's place, then >>= 1 to right shift the number by 1 bit, repeat
+
+const getAmountOfBinaryOnesIn = function(num) {
+  console.log(num.toString(2), num.toString(2).length);
+  let sum = 0;
+
+  while (num) {
+    if (num & 1) sum++;
+    num >>= 1;
+  }
+
+  return sum;
+}
