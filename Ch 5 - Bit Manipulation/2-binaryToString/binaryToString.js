e4b0f3b782272419c66707352483741f3fafd16e
Will Weindel <will.weindel@gmail.com>
2021-09-21 20:01:13 -0700

    add IOCE and pseudo to binaryToString.js

--- Ch 5 - Bit Manipulation/2-binaryToString/binaryToString.js
+++ Ch 5 - Bit Manipulation/2-binaryToString/binaryToString.js
@@ -3,6 +3,16 @@
 the binary representation. If the number cannot be represented accurately in binary with at most 32
 characters, print "ERROR:'
 */
+
+// I - a decimal (double or float)
+// O - a string representation of the decimal in binary
+// C - Time | Space
+// E - 0
+
+// decimals in binary work like 2 ** -1 (.5), 2 ** -2 (.25), 2 ** -3 (.125).... so on
+// if binary val is smaller than num, subtract binary val, add one to a cache
+// else add 0
+
 var binaryToString = function (number) {
   //bin of strings
 };
