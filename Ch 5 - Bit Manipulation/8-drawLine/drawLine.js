db56d94eafa752272fe0bec3a8faa083fb7c81d3
Will Weindel <will.weindel@gmail.com>
2021-09-23 15:12:27 -0700

    add initial solution to drawLine.js

--- Ch 5 - Bit Manipulation/8-drawLine/drawLine.js
+++ Ch 5 - Bit Manipulation/8-drawLine/drawLine.js
@@ -12,8 +12,42 @@ drawline(byte[] screen, int width, int xl, int x2, int y)
 // C - Time linear | Space linear
 // E - n/a
 
+// general idea to to find starting index and then loop through 'row'
+// if a full bit is to be changed, just vchange full byte
+
 var drawLine = function (screen, width, x1, x2, y) {
-  //Still better than I can draw irl
+  let startingIndex = (y * width);
+  let mask = (1 << x1 - 1);
+  let absolutePixel = x1;
+
+  for (let i = 1; i <= width; i++) {
+    if (x1 <= 8 * i) {
+      startingIndex += width - i;
+      break;
+    }
+  }
+
+  while (absolutePixel <= x2) {
+    if (mask > (1 << 7)) {
+      startingIndex--;
+      mask = 1;
+    }
+    setBitToOne(array, startingIndex, mask);
+    if (mask === (1 << 7) && (absolutePixel + 8) <= x2) {
+      while (absolutePixel + 8 <= x2) {
+        startingIndex--;
+        array[startingIndex] = (1 << 8) - 1;
+        absolutePixel += 8;
+      }
+      startingIndex--;
+      mask = 1;
+    } else {
+      mask <<= 1;
+      absolutePixel++;
+    }
+  }
+
+  return array;
 };
 
 module.exports = drawLine;
