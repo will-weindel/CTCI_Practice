1454070145ab11fcf2205c499cc5b4e89c8fc88f
Will Weindel <will.weindel@gmail.com>
2021-09-23 12:25:05 -0700

    add explanation to debugger.js

--- Ch 5 - Bit Manipulation/5-debugger/debugger.js
+++ Ch 5 - Bit Manipulation/5-debugger/debugger.js
@@ -2,6 +2,13 @@
 Explain what the following code does: ( ( n & ( n-1)) == 0).
 */
 
+// in order for n & m to equal 0, both ints must not share any 1's or 0's in sequence
+// starting at 1 and incrementing (2, 3, 4, etc...) you notice the only numbers that
+// return true are powers of 2 (i.e. 1, 2, 4, 8, 16 etc...). This is because these are
+// the only values were all ones are flipped:
+// 8 = 1000 | 7 = 111  8 & 7 = 0;
+// subtracting 1 in binary basically flips all rightmost zeros to 1, then flips the first 1 to 0
+
 var mysteriousFunction = function (n) {
   return (n & (n - 1)) === 0;
 };
