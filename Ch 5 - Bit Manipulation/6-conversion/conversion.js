5adbd8a37139a937cd666355454a62c35c805abc
Will Weindel <will.weindel@gmail.com>
2021-09-23 12:47:30 -0700

    add IOCE to conversion.js

--- Ch 5 - Bit Manipulation/6-conversion/conversion.js
+++ Ch 5 - Bit Manipulation/6-conversion/conversion.js
@@ -5,6 +5,13 @@ EXAMPLE
 Input: 29 (or: 11101), 15 (or: 01111)
 Output: 2
 */
+
+// I - two ints
+// O - the number of bit flips needed to convert one into two
+// C - Time O(n) where n = binary length of larger input | Space O(1)
+// E - non ints
+
+
 var conversion = function (number1, number2) {
   //convert
 };
