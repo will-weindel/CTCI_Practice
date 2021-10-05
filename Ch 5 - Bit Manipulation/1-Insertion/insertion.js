2fc2a038001bceb64d7794e036e41ec4941dd344
Will Weindel <will.weindel@gmail.com>
2021-10-04 18:06:48 -0700

    add additional solution to insertion.js

--- Ch 5 - Bit Manipulation/1-Insertion/insertion.js
+++ Ch 5 - Bit Manipulation/1-Insertion/insertion.js
@@ -34,3 +34,18 @@ const insertion = (M, N, i, j) => {
 };
 
 module.exports = insertion;
+
+const insertBinaryNumber = function(num1, num2, i, j) {
+  let mask = 1 << j - i + 1;
+
+  mask -= 1;
+  mask <<= i;
+
+  num1 |= mask;
+  num1 ^= mask;
+
+  num2 <<= i;
+  num1 |= num2;
+
+  return num1;
+}
