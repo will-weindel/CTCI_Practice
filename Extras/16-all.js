a3d3ec2b8647c68798393011b168da56e184ec2b
Will Weindel <will.weindel@gmail.com>
2021-10-12 18:57:37 -0700

    add 16.10 to 16-all

--- Extras/16-all.js
+++ Extras/16-all.js
@@ -28,3 +28,31 @@ const divide = function(a, b) {
 
   return count;
 }
+
+const findYearWithMostPeople = function(array) {
+  let currentCount = 0;
+  let maxCountAndYear = [0, 0];
+  let populationGrowthDecay = {};
+  let firstYear = 101;
+  let lastYear = -1;
+
+  for (let person of array) {
+    let pGD = populationGrowthDecay;
+
+    if (person[0] < firstYear) firstYear = person[0];
+    if (person[1] > lastYear) lastYear = person[1];
+
+    if (pGD[person[0]]) pGD[person[0]]++;
+    else pGD[person[0]] = 1;
+
+    if (pGD[person[1] + 1]) pGD[person[1] + 1]--;
+    else pGD[person[1] + 1] = -1;
+  }
+
+  for (let i = firstYear; i <= lastYear; i++) {
+    if (populationGrowthDecay[i]) currentCount += populationGrowthDecay[i];
+    if (currentCount > maxCountAndYear[0]) maxCountAndYear = [currentCount, i];
+  }
+
+  return maxCountAndYear;
+}
